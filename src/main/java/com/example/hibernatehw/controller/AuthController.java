800cd22576b750772a93e0d3efb462c10cdc75f2
Artem <riddle1994@ya.ru>
2021-11-03 10:01:16 +0300

    oauth2

--- src/main/java/com/example/hibernatehw/controller/AuthController.java
+++ src/main/java/com/example/hibernatehw/controller/AuthController.java
@@ -2,35 +2,40 @@ package com.example.hibernatehw.controller;
 
 import com.example.hibernatehw.model.Person;
 import com.example.hibernatehw.service.PersonCrudService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class EntityController {
+public class AuthController {
     private final PersonCrudService service;
 
-    public EntityController(PersonCrudService service) {
+    public AuthController(PersonCrudService service) {
         this.service = service;
     }
 
+    @Secured("ROLE_READ")
     @GetMapping("/persons/by-city")
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
         return service.findByCityOfLiving(city);
     }
 
+    @RolesAllowed("WRITE")
     @GetMapping("/persons/by-age")
     public List<Person> getPersonsByAge(@RequestParam("age") int age) {
         return service.findByAgeLessThanOrderBy(age);
     }
 
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     @GetMapping("/persons/by-nameandsurname")
     public Optional<Person> getPersonsByNameAndSurname(@RequestParam("name") String name,
                                                        @RequestParam("surname") String surname) {
         return service.findByNameAndSurname(name, surname);
     }
-
 }
---
+++ src/main/java/com/example/hibernatehw/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.hibernatehw.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true,jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Ivan")
+                .password("{noop}1234")
+                .roles("READ");
+        auth.inMemoryAuthentication()
+                .withUser("Sergey")
+                .password("{noop}4321")
+                .roles("WRITE");
+        auth.inMemoryAuthentication()
+                .withUser("Oleg")
+                .password("{noop}1111")
+                .roles("DELETE");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("by-nameandsurname", "/persons/by-city").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+}
